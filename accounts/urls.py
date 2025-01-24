9c4d9a9667521ca8a3594e4fa5a3b8153f6008fa
Aditya Kumar <ironaditya12345@gmail.com>
2025-01-24 23:40:38 +0530

    Added animal image processing and features extraction in project

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, AddPetView  ,PublicPetDashboardView, DeletePetView
+from .views import RegisterView, LoginView, ProfileView, AddPetView  ,PublicPetDashboardView, DeletePetView, SearchPetView
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('pets/add/', AddPetView.as_view(), name='add_pet'),  
+    path('pets/add/', AddPetView.as_view(), name='add_pet'), 
+    path('pets/search/', SearchPetView.as_view(), name='search_pet'), 
     path('dashboard/pets/', PublicPetDashboardView.as_view(), name='public_pet_dashboard'),
     path('pets/<int:pet_id>/delete/', DeletePetView.as_view(), name='delete_pet'),
 ]
--- animal/urls.py
+++ animal/urls.py
@@ -1,7 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/auth/', include('accounts.urls')),
     path("api/", include("animals.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
